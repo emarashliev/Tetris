59f7112c21b961748336b4f02c5d3d0432f2b6dd
Emil Marashliev <emarashliev@me.com>
2014-08-15 16:57:23 +0300

    Fisrt commit.

--- Tetris/GameViewController.swift
+++ Tetris/GameViewController.swift
@@ -2,52 +2,157 @@
 //  GameViewController.swift
 //  Tetris
 //
-//  Created by Emil Marashliev on 8/15/14.
+//  Created by Emil Marashliev on 7/31/14.
 //  Copyright (c) 2014 Indie. All rights reserved.
 //
 
 import UIKit
 import SpriteKit
 
-class GameViewController: UIViewController {
 
+class GameViewController: UIViewController, TetrisDelegate, UIGestureRecognizerDelegate {
+    
+    var scene: GameScene!
+    var tetris: Tetris!
+    
+    var panPointReference:CGPoint?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        /* Pick a size for the scene */
-        let scene = GameScene(fileNamed:"GameScene")
+        
         // Configure the view.
-        let skView = self.view as SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        let skView = view as SKView
+        skView.multipleTouchEnabled = false
         
-        /* Sprite Kit applies additional optimizations to improve rendering performance */
-        skView.ignoresSiblingOrder = true
-        
-        /* Set the scale mode to scale to fit the window */
+        // Create amd configure the scene.
+        scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         
+        scene.tick = didTick
+        
+        
+        tetris = Tetris()
+        tetris.delegate = self
+        tetris.beginGame()
+        
+        // Present the s sene.
         skView.presentScene(scene)
+        
     }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
-        } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
+    
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    
+    @IBAction func didTap(sender: UITapGestureRecognizer) {
+        tetris.rotateShape()
+    }
+    
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+
+        let currentPoint = sender.translationInView(self.view)
+        if let originalPoint = panPointReference {
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+                if sender.velocityInView(self.view).x > CGFloat(0) {
+                    tetris.moveShapeRight()
+                    panPointReference = currentPoint
+                } else {
+                    tetris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
+            }
+        } else if sender.state == .Began {
+            panPointReference = currentPoint
+        }
+    }
+    
+    
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        tetris.dropShape()
+    }
+    
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        return true
+    }
+    
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        
+        if let swipeRec = gestureRecognizer as? UISwipeGestureRecognizer {
+            if let panRec = otherGestureRecognizer as? UIPanGestureRecognizer {
+                return true
+            }
+        } else if let panRec = gestureRecognizer as? UIPanGestureRecognizer {
+            if let tapRec = otherGestureRecognizer as? UITapGestureRecognizer {
+                return true
+            }
+        }
+        return false
+    }
+    
+    
+    func didTick() {
+        tetris.letShapeFall()
+    }
+    
+    
+    func nextShape() {
+        
+        let newShapes = tetris.newShape()
+        if let fallingShape = newShapes.fallingShape {
+            self.scene.addPreviewShapeToScene(newShapes.nextShape!) {}
+            self.scene.movePreviewShape(fallingShape) {
+                self.view.userInteractionEnabled = true
+                self.scene.startTicking()
+            }
+        }
+    }
+    
+    
+    func gameDidBegin(tetris: Tetris) {
+        // The following is false when restarting a new game
+        if tetris.nextShape != nil && tetris.nextShape!.blocks[0].sprite == nil {
+            scene.addPreviewShapeToScene(tetris.nextShape!) {
+                self.nextShape()
+            }
+        } else {
+            nextShape()
+        }
+    }
+    
+    
+    func gameDidEnd(tetris: Tetris) {
+        
+        view.userInteractionEnabled = false
+        scene.stopTicking()
+    }
+    
+    
+    func gameDidLevelUp(tetris: Tetris) {
+        
+    }
+    
+    
+    func gameShapeDidDrop(tetris: Tetris) {
+        scene.stopTicking()
+        scene.redrawShape(tetris.fallingShape!) {
+            tetris.letShapeFall()
+        }
+    }
+    
+    
+    func gameShapeDidLand(tetris: Tetris) {
+        
+        scene.stopTicking()
+        nextShape()
+    }
+    
+    
+    func gameShapeDidMove(tetris: Tetris) {
+        scene.redrawShape(tetris.fallingShape!) {}
     }
 }
---
+++ Tetris/TShape.swift
@@ -0,0 +1,31 @@
+//
+//  TShape.swift
+//  Tetris
+//
+//  Created by Emil Marashliev on 8/12/14.
+//  Copyright (c) 2014 Indie. All rights reserved.
+//
+
+class TShape: Shape {
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        
+        return [
+            Orientation.Zero:       [(1, 0), (0, 1), (1, 1), (2, 1)],
+            Orientation.Ninety:     [(2, 1), (1, 0), (1, 1), (1, 2)],
+            Orientation.OneEighty:  [(1, 2), (0, 1), (1, 1), (2, 1)],
+            Orientation.TwoSeventy: [(0, 1), (1, 0), (1, 1), (1, 2)]
+            ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        
+        return [
+            Orientation.Zero:       [blocks[SecondBlockIdx], blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[FourthBlockIdx]]
+            ]
+    }
+
+}
